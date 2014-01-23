0edb053883681c542e95a9ec2278d18584fd7052
ReneMuhl <r.muhl@gmx.net>
2014-01-23 14:17:19 +0100

    Create addUser.sh

---
+++ additional/addUser.sh
@@ -0,0 +1,17 @@
+# Skript zum Hinfügen eines Nutzers auf Basis eines Keypairs
+# Verwendung: ./addUser VM_IP
+## Anmerkung: die IP der VM erhält man durch Eingabe des OpenStack-Befehls: "nova list"
+## (zum Auflisten der aktuellen VMs)
+
+if [[ -z "$1" ]]
+then 
+	echo "Verwendung: ./addUser VM_IP"
+else
+	KEYPAIRNAME="default_key"
+	VM_IP=$1
+
+	ssh root@"$VM_IP" -i "$KEYPAIRNAME".pem 'useradd guest'
+	echo "Passwort für den neuen Nutzer \"guest\" eingeben:"
+	ssh root@"$VM_IP" -i "$KEYPAIRNAME".pem 'passwd guest'
+	echo "jetzt kann sich in die VM mit dem Nutzer \"guest\" eingeloggt werden"
+fi
